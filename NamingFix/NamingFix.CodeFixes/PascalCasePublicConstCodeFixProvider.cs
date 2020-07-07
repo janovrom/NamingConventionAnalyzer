ba351bc87a312597662ec54403d554731ab31432
Roman Janovský <roman.janovsky@misterine.cz>
2020-07-07 13:21:19 +0200

    Initial commit with functional prototype for private/protected const and public const

---
+++ NamingFix/NamingFix.CodeFixes/PascalCasePublicConstCodeFixProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace NamingFix
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(PascalCasePublicConstCodeFixProvider)), Shared]
+    public class PascalCasePublicConstCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(PascalCasePublicConstAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            Diagnostic diagnostic = context.Diagnostics.First();
+            TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
+            SyntaxToken token = root.FindToken(diagnosticSpan.Start);
+
+            context.RegisterCodeFix(CodeAction.Create(CodeFixResources.CodeFixTitle, c => MakeFirstLetterUppercaseAsync(context.Document, token, c),
+                PascalCasePublicConstAnalyzer.DiagnosticId), diagnostic);
+        }
+
+        private async Task<Solution> MakeFirstLetterUppercaseAsync(Document document, SyntaxToken token, CancellationToken cancellationToken)
+        {
+            var newName = $"{char.ToUpperInvariant(token.ValueText[0])}{token.ValueText.Substring(1)}";
+
+            // Get the symbol representing the type to be renamed.
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            var symbol = semanticModel.GetDeclaredSymbol(token.Parent, cancellationToken);
+
+            // Produce a new solution that has all references to that type renamed, including the declaration.
+            var originalSolution = document.Project.Solution;
+            var optionSet = originalSolution.Workspace.Options;
+            var newSolution = await Renamer.RenameSymbolAsync(originalSolution, symbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+
+            // Return the new solution with the now-uppercase type name.
+            return newSolution;
+        }
+    }
+}
---
+++ NamingFix/NamingFix/PascalCasePublicConstAnalyzer.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace NamingFix
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PascalCasePublicConstAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "MIC001";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSymbolAction(AnalyzeField, SymbolKind.Field);
+        }
+
+        private static void AnalyzeField(SymbolAnalysisContext context)
+        {
+            var fieldSymbol = (IFieldSymbol)context.Symbol;
+
+            // If it's not const nor public, we can ignore the next check
+            if (!((fieldSymbol.IsConst || fieldSymbol.IsReadOnly) && fieldSymbol.DeclaredAccessibility == Accessibility.Public))
+                return;
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (char.IsLower(fieldSymbol.Name[0]))
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, fieldSymbol.Locations[0], fieldSymbol.Name);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
